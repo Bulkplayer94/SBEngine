ca03b270c6d87e2d340e92717f408d8cabecfef1
Bulkplayer94 <kellerkind522@gmail.com>
2024-05-31 18:00:14 +0200

    Sachen gemacht

--- SBEngine/Direct3DResources.hpp
+++ SBEngine/Direct3DResources.hpp
@@ -14,8 +14,14 @@ namespace SBEngine {
 		ID3D11DepthStencilView* d3d11DepthStencilView = nullptr;
 		ID3D11RenderTargetView1* d3d11RenderTargetView = nullptr;
 
+<<<<<<< Updated upstream
 		bool Init();
 		bool Resize();
 		bool Release();
+=======
+		bool Init(HWND hWnd);
+		bool Resize(bool firstTime = false)
+
+>>>>>>> Stashed changes
 	} extern Direct3DResources;
 }
